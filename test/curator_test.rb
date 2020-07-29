7ed40d805505698cf2606d58bd915395e44f2b86
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-28 20:09:49 -0600

    Add artist test added

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -13,6 +13,7 @@ class CuratorTest < Minitest::Test
   def test_it_has_attributes
     curator = Curator.new
     assert_equal [], curator.photographs
+    assert_equal [], curator.artists
   end
 
   def test_add_photograph
@@ -29,6 +30,23 @@ class CuratorTest < Minitest::Test
     curator.add_photograph(photo_2)
 
     assert_equal [photo_1, photo_2], curator.photographs
+  end
+
+  def test_add_artist
+    artist_1 = Artist.new({ id: "1",
+                            name: "Henri Cartier-Bresson",
+                            born: "1908",
+                            died: "2004",
+                            country: "France"})
+    artist_2 = Artist.new({ id: "2",
+                            name: "Ansel Adams",
+                            born: "1902",
+                            died: "1984",
+                            country: "United States"})
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_1)
+
+    assert_equal [artist_1, artist_2], curator.artists
   end
 
 
