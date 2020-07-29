d496006e02e04c2f2f822c478d7c104ee1c8fc48
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-28 19:51:47 -0600

    Two tests running

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+
+  attr_reader :id,
+              :name,
+              :born,
+              :died,
+              :country
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+
+
+
+
+
+
+
+end
